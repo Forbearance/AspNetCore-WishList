d11bac41e687ec3d2150c8a518ac0ef0b4a927bf
Jerry Z Wu <jwuster@gmail.com>
2022-02-02 20:06:23 -0800

    fix Configure method

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -32,8 +32,8 @@ namespace WishList
                 app.UseExceptionHandler("/Home/Error");
             }
 
-            app.UseRouting()
-                .UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
+            app.UseRouting();
+            app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
         }
     }
 }
